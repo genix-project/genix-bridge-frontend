7c6f22a5a5225f06305f446edbdde813c442c5e2
Twinky-kms <twinkykms@gmail.com>
2024-05-06 14:34:27 +0000

    more

--- src/App.jsx
+++ src/App.jsx
@@ -74,9 +74,6 @@ export default function App() {
                     <h4 className="title text-center">
                       Convert to BEP-20 token.
                     </h4>
-                    <h6 className="text-center">
-                      {/* Stable, Realiable & Great Exchange Rate */}
-                    </h6>
                   </div>
                 </div>
               </div>
@@ -86,11 +83,7 @@ export default function App() {
                   <div className="d-flex flex-column me-auto mt-4 mb-auto">
                     <h2 className="title text-center">
                       Convert to BEP-20 token.
-                    </h2><br />
-                    <h4 className="text-center">
-                      {/* Stable, Realiable & Great Exchange Rate */}
-                    </h4>
-
+                    </h2>
                   </div>
                 </div>
               </div>
@@ -110,16 +103,8 @@ export default function App() {
                     This portal will remain open for N !timeframe!. Once deposit is made you will be able to mint your tokens
                     in 1~ month. If you have any questions please contact us in Discord. ~insert link here~
                   </p>
-                  {/* <p>A wrapped crypto token is a converted version of a cryptocurrency or asset that functions on a network other
-                    than the original asset’s blockchain. Each wrapped token has the same value as the asset it represents and is
-                    easily interchangeable.</p>
-                  <p>Wrapped Coins essentially represent crypto assets on non-native blockchains. These coins
-                    are “wrapped” because they are inserted into a wrapper or digital vault that allows the wrapped version to operate
-                    on a different blockchain.
-                  </p> */}
                 </h5>
                 <br />
-                {/* <h5 className="mb-3 text-center">Exchange Rate</h5> */}
                 <br />
                 <h5 className="mb-3 text-center">
                   <p className="mb-0">
@@ -136,22 +121,6 @@ export default function App() {
                   >
                     Enter
                   </button>
-                  {/* <DropdownButton
-                    title={
-                      controller === null
-                        ? "Select Wrapped Network"
-                        : controller
-                    }
-                    className="mb-2"
-                  >
-                    <Dropdown.Item
-                      onClick={() => {
-                        setController("Binance Smart Chain (BSC)");
-                      }}
-                    >
-                      Binance Smart Chain (BSC)
-                    </Dropdown.Item>
-                  </DropdownButton> */}
                 </h5>
               </FadeInSection>
             </Container>
